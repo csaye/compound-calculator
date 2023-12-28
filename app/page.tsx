be3894f72a9a2ac39a2544fea65cbaee9efe7960
Cooper Saye <coopersaye@gmail.com>
2023-12-28 15:34:52 -0800

    Create form inputs

--- app/page.tsx
+++ app/page.tsx
@@ -2,17 +2,30 @@
 import { Chart, LinearScale, PointElement } from 'chart.js'
 import { Scatter } from 'react-chartjs-2'
 import styles from './page.module.scss'
+import { useState } from 'react'
+import { NumberInput } from '@/components/NumberInput'
 
 Chart.register(LinearScale, PointElement)
 
 export default function Index() {
+  const [startingAmount, setStartingAmount] = useState<number | null>(null)
+  const [totalYears, setTotalYears] = useState<number | null>(null)
+  const [dailyAddition, setDailyAddition] = useState<number | null>(null)
+  const [weeklyAddition, setWeeklyAddition] = useState<number | null>(null)
+  const [monthlyAddition, setMonthlyAddition] = useState<number | null>(null)
+  const [yearlyAddition, setYearlyAddition] = useState<number | null>(null)
+  const [dailyInterest, setDailyInterest] = useState<number | null>(null)
+  const [weeklyInterest, setWeeklyInterest] = useState<number | null>(null)
+  const [monthlyInterest, setMonthlyInterest] = useState<number | null>(null)
+  const [yearlyInterest, setYearlyInterest] = useState<number | null>(null)
+
   return (
     <div className={styles.container}>
       <div className={styles.title}>
         <h1>Compound Interest Calculator</h1>
       </div>
       <div className={styles.content}>
-        <div className={styles.form}></div>
+        {renderForm()}
         <div className={styles.plot}>
           <Scatter
             width='300'
@@ -47,4 +60,63 @@ export default function Index() {
       </div>
     </div>
   )
+
+  function renderForm() {
+    return (
+      <div className={styles.form}>
+        <div>
+          <NumberInput
+            format='money'
+            placeholder='Starting amount'
+            onChange={setStartingAmount}
+          />
+          <NumberInput placeholder='Total years' onChange={setTotalYears} />
+        </div>
+        <div>
+          <NumberInput
+            format='money'
+            placeholder='Daily addition'
+            onChange={setDailyAddition}
+          />
+          <NumberInput
+            format='money'
+            placeholder='Weekly addition'
+            onChange={setWeeklyAddition}
+          />
+          <NumberInput
+            format='money'
+            placeholder='Monthly addition'
+            onChange={setMonthlyAddition}
+          />
+          <NumberInput
+            format='money'
+            placeholder='Yearly addition'
+            onChange={setYearlyAddition}
+          />
+        </div>
+        <div>
+          <NumberInput
+            format='percent'
+            placeholder='Daily interest'
+            onChange={setDailyInterest}
+          />
+          <NumberInput
+            format='percent'
+            placeholder='Weekly interest'
+            onChange={setWeeklyInterest}
+          />
+          <NumberInput
+            format='percent'
+            placeholder='Monthly interest'
+            onChange={setMonthlyInterest}
+          />
+          <NumberInput
+            format='percent'
+            placeholder='Yearly interest'
+            onChange={setYearlyInterest}
+          />
+        </div>
+      </div>
+    )
+  }
 }
---
+++ components/NumberInput.tsx
@@ -0,0 +1,75 @@
+import { DetailedHTMLProps, InputHTMLAttributes, useState } from 'react'
+
+type Props = {
+  format?: 'money' | 'percent'
+  onChange: (value: number | null) => void
+  placeholder?: string
+}
+
+export function NumberInput({ format, onChange, placeholder }: Props) {
+  const [rawString, setRawString] = useState('')
+
+  return (
+    <input
+      value={rawString}
+      onChange={(e) => onRawStringChange(e.target.value)}
+      placeholder={placeholder}
+    />
+  )
+
+  function onRawStringChange(rawInput: string) {
+    let sanitizedInput = getSanitized(rawInput)
+    // ignore trailing % if deleting from end
+    if (rawString.endsWith('%') && !rawInput.includes('%')) {
+      sanitizedInput = sanitizedInput.slice(0, -1)
+      console.log(sanitizedInput)
+    }
+
+    const num = Number(sanitizedInput)
+
+    if (!sanitizedInput || isNaN(num)) {
+      if (rawString) {
+        setRawString('')
+        onChange(null)
+      }
+      return
+    }
+
+    const nextRawString = formatNumber()
+    if (nextRawString !== rawString) {
+      setRawString(nextRawString)
+      onChange(num)
+    }
+
+    function getSanitized(num: string) {
+      num = num.replace(/[^0-9\.]/g, '')
+      // remove all but first period
+      if (num.includes('.')) {
+        const parts = num.split('.')
+        num = [parts[0], parts.slice(1).join('')].join('.')
+      }
+      return num
+    }
+
+    function formatNumber() {
+      let numFormatted = num.toLocaleString('en-US')
+
+      if (format === 'money') {
+        numFormatted = num.toLocaleString('en-US', {
+          style: 'currency',
+          currency: 'USD',
+          minimumFractionDigits: 0,
+        })
+      }
+
+      // keep period at end of formatted number
+      if (rawInput.endsWith('.') && !numFormatted.includes('.')) {
+        numFormatted += '.'
+      }
+
+      if (format === 'percent') numFormatted += '%'
+
+      return numFormatted
+    }
+  }
+}
